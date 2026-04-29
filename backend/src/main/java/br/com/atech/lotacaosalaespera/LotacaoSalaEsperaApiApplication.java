d1f901b1e7c9bffd2e2e8812bedfdfbdd9223af3
andreick <andreick.gomes.ismart@gmail.com>
2026-04-28 21:27:36 -0300

    chore: inicializa o backend com Spring Boot e Maven

---
+++ backend/src/main/java/br/com/atech/lotacaosalaespera/LotacaoSalaEsperaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.atech.lotacaosalaespera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotacaoSalaEsperaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotacaoSalaEsperaApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/atech/lotacaosalaespera/LotacaoSalaEsperaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.atech.lotacaosalaespera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotacaoSalaEsperaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
